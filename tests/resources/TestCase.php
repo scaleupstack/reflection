7c3253382f9b8d2ac2ff9c661b6be175ee14fdd1
Thomas Nunninger <thomas.nunninger@scaleupventures.com>
2019-02-17 11:09:44 +0100

    Initial project setup.

---
+++ tests/resources/TestCase.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types = 1);
+
+/**
+ * This file is part of ScaleUpStack/Reflection.
+ *
+ * For the full copyright and license information, please view the README.md and LICENSE.md files that were distributed
+ * with this source code.
+ *
+ * @copyright 2019 - present ScaleUpVentures GmbH, https://www.scaleupventures.com
+ * @link      https://github.com/scaleupstack/reflection
+ */
+
+namespace ScaleUpStack\Reflection\Tests;
+
+final class TestCase extends \PHPUnit\Framework\TestCase
+{
+}
