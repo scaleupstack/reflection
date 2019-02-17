8f701711759f046d457be59f179788be2a93f3b9
Thomas Nunninger <thomas.nunninger@scaleupventures.com>
2019-02-17 11:33:32 +0100

    Added project-specific Assert object.

---
+++ src/Assert.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types = 1);
+
+/**
+ * This file is part of ScaleUpStack/Reflection.
+ *
+ * For the full copyright and license information, please view the README.md and LICENSE.md files that were distributed
+ * with this source code.
+ *
+ * @copyright 2019 - present ScaleUpVentures GmbH, https://www.scaleupventures.com
+ * @link      https://github.com/scaleupstack/reflection
+ */
+
+namespace ScaleUpStack\Reflection;
+
+final class Assert extends \ScaleUpStack\Assert\Assert
+{
+    protected static $assertionClassName = InvalidArgumentException::class;
+}
